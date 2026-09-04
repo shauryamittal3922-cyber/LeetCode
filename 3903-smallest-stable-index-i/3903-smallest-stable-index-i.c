9e523cd7c9ef99288fdb10d6c63ffb9b56e344f1
shauryamittal3922-cyber <shauryamittal3922@gmail.com>
2026-09-04 12:00:10 +0530

    Time: 0 ms (100%), Space: 10.4 MB (18.75%) - LeetHub

---
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.c
@@ -0,0 +1,21 @@
+int firstStableIndex(int* nums, int numsSize, int k) {
+    int* l_max = (int *)malloc(numsSize * sizeof(int));
+    int* r_min = (int *)malloc(numsSize * sizeof(int));
+
+    l_max[0] = nums[0];
+    r_min[numsSize - 1] = nums[numsSize - 1];
+
+    for(int i = 1; i < numsSize; i++){
+        l_max[i] = (nums[i] > l_max[i-1])? nums[i] : l_max[i-1];
+    }
+
+    for(int i = numsSize -2; i >= 0; i--){
+        r_min[i] = (nums[i] < r_min[i+1])? nums[i] : r_min[i+1];
+    }
+
+    for(int i = 0; i < numsSize; i++){
+        if(l_max[i] - r_min[i] <= k) return i;
+    }
+
+    return -1;
+}
