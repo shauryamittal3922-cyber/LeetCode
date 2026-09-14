f6792b2ada4cad8b6e766c307a0b1bf222ee3dd5
shauryamittal3922-cyber <shauryamittal3922@gmail.com>
2026-09-14 21:50:59 +0530

    Time: 0 ms (100%), Space: 8.6 MB (54.43%) - LeetHub

---
+++ 0836-rectangle-overlap/0836-rectangle-overlap.c
@@ -0,0 +1,5 @@
+bool isRectangleOverlap(int* rec1, int rec1Size, int* rec2, int rec2Size) {
+    if( (fmax(rec1[0], rec2[0]) < fmin(rec1[2], rec2[2])) && (fmax(rec1[1], rec2[1]) < fmin(rec1[3], rec2[3])) ) return true;
+
+    return false;
+}
