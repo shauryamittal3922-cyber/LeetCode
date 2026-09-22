1488136e74064c2fbb275162cae5306ef9944a73
shauryamittal3922-cyber <shauryamittal3922@gmail.com>
2026-09-22 08:41:28 +0530

    Time: 0 ms (100%), Space: 9.3 MB (79.5%) - LeetHub

---
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.c
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+
+void traverse(struct TreeNode* root, int* res, int* returnSize){
+    if(root == NULL) return;
+
+    traverse(root->left, res, returnSize);
+    traverse(root->right, res, returnSize);
+    res[(*returnSize)++] = root->val;
+}
+
+int* postorderTraversal(struct TreeNode* root, int* returnSize) {
+    int* result = malloc(100 * sizeof(int));
+    * returnSize = 0;
+
+    traverse(root, result, returnSize);
+
+    return result;
+}
